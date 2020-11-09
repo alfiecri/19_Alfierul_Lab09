e678faec6e4c0c7d09480caee54eeff060200f5d
alfiecri <jediknightalfie@gmail.com>
2020-11-09 11:58:52 +0800

    Initial commit

---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour
+{
+    public GameObject coordsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().coordsText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
